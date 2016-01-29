24b7c77dade3db089ffab2afd15b2d6d5258ce9c
murakami <tmaqua007@gmail.com>
2016-01-29 15:46:54 +0900

    itemクラスの作成

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -3,13 +3,12 @@ require 'item.rb'
 RSpec.describe(Item) do
   it do
     item = Item.new
+    item.name = "name"
+    item.price = 100
 
     # 名前nameを持っている
-    expect(item.name).to eq(true)
+    expect(item.name).to eq("name")
     # 価格priceを持っている
-    expect(item.price).to eq(true)
-
-    # expect(greeter.greet).to eq("hello, world")
-    # expect(greeter.greet("ruby")).to eq("hello, ruby")
+    expect(item.price).to eq(100)
   end
 end
